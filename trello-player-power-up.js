bf7dc4c01c2a30a7e622746e580d53ec332104bd
Viktor Jevdokimov <vjevdokimov@gmail.com>
2024-06-28 10:15:21 +0300

    Update trello-player-power-up.js

--- trello-player-power-up.js
+++ trello-player-power-up.js
@@ -1,1 +1,13 @@
 //
+var Promise = TrelloPowerUp.Promise;
+
+TrelloPowerUp.initialize({
+  'board-buttons': function(t, options){
+    return [{
+      icon: './trello-player-192.png',
+      text: 'Trello Player',
+    }];
+  },
+}, {
+  helpfulStacks: true
+});
