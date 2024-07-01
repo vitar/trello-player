7e0f1ef87e222159f564dafb18542c73d9c79563
Viktor Jevdokimov <vjevdokimov@gmail.com>
2024-07-01 17:09:10 +0300

    Update trello-player-power-up.js

--- trello-player-power-up.js
+++ trello-player-power-up.js
@@ -8,6 +8,12 @@ window.TrelloPowerUp.initialize({
           title: "Trello Player",
           url: "./trello-player-power-up-popup.html",
           fullscreen: false,
+          actions: [{
+            icon: './trello-player-192.png',
+            url: 'https://github.com/vitar/trello-player',
+            alt: 'Trello Player',
+            position: 'left',
+          }],
         });
       },
     }];
