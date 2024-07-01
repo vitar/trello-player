471c7965121e2ee3529e555670ab1bd150f1a3eb
Viktor Jevdokimov <vjevdokimov@gmail.com>
2024-07-01 12:37:55 +0300

    Update trello-player-power-up-popup.js

--- trello-player-power-up-popup.js
+++ trello-player-power-up-popup.js
@@ -1,1 +1,13 @@
-var t = window.TrelloPowerUp.iframe();
+const t = window.TrelloPowerUp.iframe();
+let currentAttachmentIndex = 0;
+let m4aAttachments = [];
+let audioPlayer = document.getElementById('audio-player');
+
+function loadPlayer() {
+  const listInfo = t.list('cards');
+  for(const card of listInfo.cards) {
+    console.log(card.id);
+  }
+}
+
+window.onload = loadPlayer();
