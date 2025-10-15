73ed0aef3a3a1906a1457f153808fb2e8ada7954
vitar <vjevdokimov@gmail.com>
2025-10-15 05:07:01 +0000

    Deploying to gh-pages from @ vitar/trello-player@4d85140e65e0a4412d37212466ee14a532587e62 🚀

--- preview/codex/refactor-playback-controls-and-add-ab-repeat-button-71lsit/trello-player-power-up-popup.js
+++ preview/codex/refactor-playback-controls-and-add-ab-repeat-button-71lsit/trello-player-power-up-popup.js
@@ -95,7 +95,13 @@ class WaveformPreview extends HTMLElement {
     this.regionsPlugin = null;
     const abortController = new AbortController();
     const signal = abortController.signal;
-    const shouldLoadRegions = Boolean(WaveSurfer?.RegionsPlugin?.create);
+    const plugins = [];
+    if (WaveSurfer?.Regions?.create) {
+      this.regionsPlugin = WaveSurfer.Regions.create({
+        dragSelection: false
+      });
+      plugins.push(this.regionsPlugin);
+    }
     const mergedOptions = {
       container: this.canvas,
       height: 80,
@@ -104,20 +110,13 @@ class WaveformPreview extends HTMLElement {
       ...options
     };
     const optionPlugins = Array.isArray(mergedOptions.plugins) ? mergedOptions.plugins : [];
-    if (optionPlugins.length > 0) {
-      mergedOptions.plugins = [...optionPlugins];
+    if (plugins.length > 0 || optionPlugins.length > 0) {
+      mergedOptions.plugins = [...optionPlugins, ...plugins];
     }
     this.wavesurfer = WaveSurfer.create(mergedOptions);
     this.wavesurfer.on('destroy', () => {
       abortController.abort();
     });
-    if (shouldLoadRegions && this.wavesurfer?.registerPlugin) {
-      this.regionsPlugin = this.wavesurfer.registerPlugin(
-        WaveSurfer.RegionsPlugin.create({
-          dragSelection: false
-        })
-      );
-    }
     if (this.regionsPlugin) {
       this.regionsPlugin.on('region-updated', (region) => {
         if (this.abLoopRegion && region && region.id === this.abLoopRegion.id) {
