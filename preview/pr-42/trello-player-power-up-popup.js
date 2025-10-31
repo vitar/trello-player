e873676ff57a2c7f281abbfa8da049bb515b2377
vitar <vitar@users.noreply.github.com>
2025-10-31 11:10:08 +0000

    Deploying to gh-pages from @ vitar/trello-player@9fa0e72f03318f7bae695d4ccd99f7dca54f895e 🚀

--- preview/pr-42/trello-player-power-up-popup.js
+++ preview/pr-42/trello-player-power-up-popup.js
@@ -1283,6 +1283,20 @@ function createPlayerController(abLoop) {
       abLoop.enforceAbLoop();
     });
 
+    dom.audioPlayer.addEventListener('canplay', () => {
+      const loadToken = Number(dom.audioPlayer.dataset.loadToken);
+      if (loadToken !== state.currentLoadRequest) {
+        return;
+      }
+      state.isAudioLoaded = true;
+      updateSettingsControlsAvailability();
+    });
+
+    dom.audioPlayer.addEventListener('emptied', () => {
+      state.isAudioLoaded = false;
+      updateSettingsControlsAvailability();
+    });
+
     dom.audioPlayer.addEventListener('loadstart', () => {
       clearSoundtouchBuffers();
       updatePlayPauseButton();
